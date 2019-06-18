d8b1681ed638aa070de00b07e0d372bd080daa69
Julian <jfigueroa@moorea.io>
2019-06-18 10:40:51 -0300

    Antes de eliminar botones de addRow y Add col

--- src/App.js
+++ src/App.js
@@ -8,7 +8,48 @@ export default class App extends React.Component {
 		};
 	}
 
+	remove = (axis) => {
+		let board = [ ...this.state.board ];
+
+		switch (axis) {
+			case 'y':
+				if (board.length > 1) {
+					board.pop();
+				}
+				break;
+
+			case 'x':
+				board.forEach((row) => {
+					if (row.length > 1) {
+						row.pop();
+					}
+				});
+				break;
+
+			default:
+		}
+		this.setState({ board });
+	};
+
+	add = (axis) => {
+		let board = [ ...this.state.board ];
+
+		switch (axis) {
+			case 'y':
+				let newRow = board[0].map(() => 0);
+				board.push(newRow);
+				break;
+
+			case 'x':
+				board.forEach((row) => row.push(0));
+				break;
+
+			default:
+		}
+		this.setState({ board });
+	};
+
 	render() {
-		return <Grid board={this.state.board} />;
+		return <Grid add={this.add} remove={this.remove} board={this.state.board} />;
 	}
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,77 +5,33 @@ import '../App.css';
 export default class Grid extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			boardGrid: this.props.board
-		};
 	}
 
-	remove = (axis) => {
-		let boardGrid = [ ...this.state.boardGrid ];
-
-		switch (axis) {
-			case 'y':
-				if (boardGrid.length > 1) {
-					boardGrid.pop();
-				}
-				break;
-
-			case 'x':
-				boardGrid.forEach((row) => {
-					if (row.length > 1) {
-						row.pop();
-					}
-				});
-				break;
-
-			default:
-		}
-		this.setState({ boardGrid });
-	};
-
-	add = (axis) => {
-		let boardGrid = [ ...this.state.boardGrid ];
-
-		switch (axis) {
-			case 'y':
-				let newRow = boardGrid[0].map(() => 0);
-				boardGrid.push(newRow);
-				break;
-
-			case 'x':
-				boardGrid.forEach((row) => row.push(0));
-				break;
-
-			default:
-		}
-		this.setState({ boardGrid });
-	};
-
 	render() {
-		const colNum = this.state.boardGrid[0].length;
-		const rowNum = this.state.boardGrid.length;
+		const colNum = this.props.board[0].length;
+		const rowNum = this.props.board.length;
 
 		return (
 			<div className="Container">
 				<div className="Wrapper">
 					<BoardGrid dimensions={this.props.board} />
 					<div className="RowButtons">
-						<button onClick={() => this.remove('y')} className="Btn">
+						<button onClick={() => this.props.remove('y')} className="Btn">
 							-
 						</button>
 						<input value={rowNum} type="number" />
-						<button onClick={() => this.add('y')} className="Btn">
+						<button onClick={() => this.props.add('y')} className="Btn">
 							+
 						</button>
 					</div>
 				</div>
 
 				<div className="ColumnButtons">
-					<button onClick={() => this.remove('x')} className="Btn">
+					<button onClick={() => this.props.remove('x')} className="Btn">
 						-
 					</button>
 					<input value={colNum} type="number" />
-					<button onClick={() => this.add('x')} className="Btn">
+					<button onClick={() => this.props.add('x')} className="Btn">
 						+
 					</button>
 				</div>
